7c37398e7c7e4c4ef4173ac9962d185b85be40f7
Omprakash Rawat <omprakash824rawat@gmail.com>
2023-09-28 16:22:00 +0530

    Knowlege Added

---
+++ Knowledge/logic print.py
@@ -0,0 +1,22 @@
+from logic import *
+
+
+bus = Symbol('Bus') # Bus busStop pe ayegi
+college = Symbol('College') # Will go to college
+canteen = Symbol('Canteen') # Will go to canteen
+chai = Symbol('Chai')
+poha = Symbol('Poha')
+Sentence = Implication(Or(college, canteen), bus)
+
+know = And(
+            Implication(Or(chai, poha),canteen),
+            Implication(Or(college, canteen), bus),
+            Implication(canteen, college),
+            Not(bus),
+            Implication(canteen, college),
+            Implication(Not(bus), Not(college))
+)
+
+print(model_check(know, poha))
+
+print(Sentence.formula())
