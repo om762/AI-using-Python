698b465b9a2b7f2f7658a65ba76f717e07f868d8
Omprakash Rawat <omprakash824rawat@gmail.com>
2023-11-04 20:36:34 +0530

    ALL Done

--- Projects/Stocks/Stocks.py
+++ Projects/Stocks/Stocks.py
@@ -33,9 +33,9 @@ model.add_edge(model.start, market_up, 0.6)
 model.add_edge(model.start, market_down, 0.4)
 
 # Transitional Model
-model.add_edge(market_up, market_up, 0.9)  # If today's market is up then probability of tommorow market will up
-model.add_edge(market_up, market_down, 0.1) # If today's market is up then probability of tommorow will be go down
-model.add_edge(market_down, market_down, 0.1) # If today is market is down then probability of tommorow will be market go down too
-model.add_edge(market_down, market_up, 0.9) # If today is market is down then probability of tommorow will not go down i.e. it will go up
+model.add_edge(market_up, market_up, 0.8)  # If today's market is up then probability of tommorow market will up
+model.add_edge(market_up, market_down, 0.2) # If today's market is up then probability of tommorow will be go down
+model.add_edge(market_down, market_down, 0.5) # If today is market is down then probability of tommorow will be market go down too
+model.add_edge(market_down, market_up, 0.5) # If today is market is down then probability of tommorow will not go down i.e. it will go up
 
 # Just play around probability of Transitional Model
