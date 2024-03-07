2f894df74110b89d3b4be0d257dcfa2b560bc764
Omprakash Rawat <omprakash824rawat@gmail.com>
2024-03-07 16:07:00 +0530

    gitignore update

--- 6. Language/vectors/vectors.py
+++ 6. Language/vectors/vectors.py
@@ -12,7 +12,9 @@ with open("words.txt", encoding="utf-8") as f:
         words[word] = vector
 
 
-def distance(w1, w2):
+def distance(word1, word2):
+    w1 = words[word1]
+    w2 = words[word2]
     return cosine(w1, w2)
 
 
